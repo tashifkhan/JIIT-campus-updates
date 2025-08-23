cfff36b9d6930252946ce9c033e8b418065da0af
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-08-23 23:17:36 +0530

    added analytics

--- campus-updates/app/layout.tsx
+++ campus-updates/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<ThemeProvider>{children}</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
